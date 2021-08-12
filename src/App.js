51911f5a01dd1956dba24b57e84129a95f88b0d6
TCM20 <78640690+TCM20@users.noreply.github.com>
2021-08-12 16:49:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              Store
+         Casual Clothing
             </Link>
           </div>
           <div>
